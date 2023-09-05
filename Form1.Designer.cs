7c2e431389ed1c991cccd1e1d978327b6b531914
Irfan <irfaanacar@gmail.com>
2023-09-06 00:59:43 +0300

    CSharp psychoanalysis test questions coding
    I coded a psychoanalysis test application that determines the character score based on the answers given in this application. By the way, sorry for my english

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace PsikoAnalizTestUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(541, 210);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button1.Location = new System.Drawing.Point(12, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(541, 52);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button2.Location = new System.Drawing.Point(12, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(541, 52);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button3.Location = new System.Drawing.Point(12, 411);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(541, 52);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button4.Location = new System.Drawing.Point(12, 469);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(541, 52);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button5.Location = new System.Drawing.Point(12, 527);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(541, 52);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.Font = new System.Drawing.Font("Oswald", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(124, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 48);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "KARAKTER ANALİZ TESTİ";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Silver;
+            this.button6.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button6.Location = new System.Drawing.Point(559, 353);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(541, 52);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
+            this.button7.Location = new System.Drawing.Point(559, 411);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(541, 52);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.button8.Location = new System.Drawing.Point(636, 106);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(132, 76);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "BAŞLA";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Crimson;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.button9.Location = new System.Drawing.Point(636, 106);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(132, 76);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "BİTİR";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(621, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Toplam Puan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.label3.Location = new System.Drawing.Point(790, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 29);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "SORU:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(106, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 29);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(1108, 635);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PsikoAnalizTestUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=IRFAN\\SQLEXPRESS; Initial Catalog=karaktertest;Integrated Security=True");
+        private void butonlaripasifyap()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+        }
+        private void butonlariaktifyap()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+        }
+        int soru;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button9.Visible = false;
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = false;
+            butonlariaktifyap();
+            button8.Text = "İLERİ";
+            soru++;
+            label5.Text=soru.ToString();
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            if (soru == 1)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru1 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+
+                }
+                baglan.Close();
+            }
+            if (soru == 2)
+            {
+                button4.Visible = true;
+                button5.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru2 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    button5.Text = (oku["e"].ToString());
+                }
+                baglan.Close();
+            }
+            if (soru == 3)
+            {
+                button4.Visible = true;
+                button5.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru3 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    button5.Text = (oku["e"].ToString());
+                }
+                baglan.Close();
+            }
+            if (soru == 4)
+            {
+                button4.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru4 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                }
+                baglan.Close();
+            }
+            if (soru == 5)
+            {
+                button4.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru5 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                }
+                baglan.Close();
+            }
+            if (soru == 6)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru6 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button2.Text = (oku["c"].ToString());
+
+                }
+                baglan.Close();
+            }
+            if (soru == 7)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru7 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                }
+                baglan.Close();
+            }
+            if (soru == 8)
+            {
+                button4.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                button7.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru8 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    button5.Text = (oku["e"].ToString());
+                    button6.Text = (oku["f"].ToString());
+                    button7.Text = (oku["g"].ToString());
+
+                }
+                baglan.Close();
+            }
+
+            if (soru == 9)
+            {
+                button4.Visible = true;
+                button5.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru9 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    button5.Text = (oku["e"].ToString());
+                }
+                baglan.Close();
+            }
+            if (soru == 10)
+            {
+                button8.Visible = false;
+                button9.Visible = true;
+                button4.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru10 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    richTextBox1.Text = (oku["soru"].ToString());
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    button5.Text = (oku["e"].ToString());
+                    button6.Text = (oku["f"].ToString());
+
+                }
+                baglan.Close();
+            }
+        }
+        int toplam=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = true;
+            butonlaripasifyap();
+            if(soru==1) //BU YAPILANLAR YALNIZCA A ŞIKKI İÇİN GEÇERLİYDİ.
+            {
+                toplam = toplam + 2;
+            }
+            if(soru==2 || soru==5 || soru == 6 || soru == 7 || soru == 8)
+            {
+                toplam = toplam + 6;
+            }
+            if(soru==3 || soru ==4 || soru ==10)
+            {
+                toplam = toplam + 4;
+            }
+            if (soru == 9)
+            {
+                toplam = toplam + 7;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = true;
+
+            butonlaripasifyap();
+
+            if (soru==1 || soru ==2 || soru ==5 || soru == 6)//BU YAPILANLAR YALNIZCA B ŞIKKI İÇİN GEÇERLİYDİ.
+            {
+                toplam=toplam + 4;
+            }
+            if (soru == 3 || soru == 7 || soru == 10)
+            {
+                toplam = toplam + 2;
+            }
+            if (soru==4 || soru == 9)
+                {
+                  toplam = toplam + 6;
+            }if(soru==8)
+            {
+                toplam = toplam + 7;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = true;
+
+            butonlaripasifyap();
+
+            if (soru == 1)//BU YAPILANLAR YALNIZCA C ŞIKKI İÇİN GEÇERLİYDİ.
+            {
+                toplam = toplam + 6;
+            }
+            if (soru == 2)
+            {
+                toplam = toplam + 7;
+
+            }if(soru == 3 || soru == 8)
+            {
+                toplam= toplam + 5;
+            }
+            if(soru==4 || soru == 6)
+            {
+                toplam = toplam + 2;
+            }if(soru==5 || soru == 10)
+            {
+                toplam= toplam+3;
+            }
+            if(soru==7 || soru == 9)
+            {
+                toplam = toplam+4;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = true;
+
+            butonlaripasifyap();
+
+            if (soru == 2 || soru ==9) //BU YAPILANLAR YALNIZCA D ŞIKKI İÇİN GEÇERLİYDİ.
+            {
+                toplam = toplam + 2;
+            }
+            if(soru == 3)
+            {
+                toplam = toplam + 7;
+            }
+            if (soru == 4)
+            {
+                toplam = toplam + 1;
+            }
+            if(soru==5 || soru == 10)
+            {
+                toplam = toplam + 5;
+            }
+            if (soru == 8)
+            {
+                toplam = toplam + 4;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = true;
+
+            butonlaripasifyap();
+
+            if (soru==2 || soru ==9)
+            {
+                toplam = toplam + 1;
+            }
+            if (soru == 3 || soru ==10)
+            {
+                toplam = toplam + 6;
+            }
+            if (soru == 5)
+            {
+                toplam = toplam + 2;
+            }
+            if (soru == 8)
+            {
+                toplam = toplam + 3;
+            }     
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = true;
+
+            butonlaripasifyap();
+
+            if (soru == 8)
+            {
+                toplam = toplam + 2;
+            }
+            if (soru == 10)
+            {
+                toplam = toplam + 1;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        
+        {
+            button8.Enabled = true;
+
+            butonlaripasifyap();
+            toplam += 1;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if(toplam>=31 && toplam <= 40)
+            {
+                richTextBox1.Text = "İnsanlar sizi mantıklı, ihtiyatlı, dikkatli ve pratik birisi olarak görürler. Sizi zeki, yetenekli ve hünerli ama alçak gönüllü olarak tanırlar. Çok hızlı arkadaşlık kurmayan, ama arkadaşlarına karşı çok sadık olan ve onlardan da aynı şeyi bekleyen birisiniz.";
+            }
+            if(toplam>=41 && toplam <= 50)
+            {
+                richTextBox1.Text = "İnsanlar sizi taze, canlı, çekici, eğlendirici, pratik ve daima ilginç birisi olarak görürler; her zaman ilgi odağı olan ama çok aşırıya kaçmayacak kadar da dengeli birisi.. İnsanlar sizi ayrıca iyiliksever, düşünceli, anlayışlı ve kendilerini neşelendiren ve rahatlatan birisi olarak tanırlar.";
+            }
+            if(toplam>=51 && toplam <= 60)
+            {
+                richTextBox1.Text = "insanlar sizi heyecan verici, havai, düşüncesiz yapıda, doğal liderlik özellikleri olan, her zaman doğru olmasa da hızlı karar veren birisi olarak tanırlar. Seni cesur, maceraperest birisi olarak tanırlar; her şeyi bir kez denemek isteyen, macera yaşamak için fırsatları kaçırmayan birisi.. Yaydığınız heyecandan dolayı insanlar sizinle aynı iş yerinde yaşamaktan zevk alırlar.";
+            }
+            if (toplam >= 60)
+            {
+                richTextBox1.Text = "İnsanlar sana kırılgan bir eşya muamelesi yapıyorlar. Kibirli, bencil ve aşırı baskın birisi olarak görülüyorsun. İnsanlar size hayranlık duyup sizin gibi olmak isteyebilirler ama size her zaman güvenmezler ve sizinle çok yakın ilişkide olmaktan kaçınırlar.";
+            }
+            label3.Text = toplam.ToString();
+        }
+    } 
+}
